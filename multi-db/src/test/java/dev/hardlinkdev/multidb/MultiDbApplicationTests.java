923ad9de4970ecdc5bc79fff696d4aea3489c148
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-15 14:04:50 +0500

    initital commit

---
+++ multi-db/src/test/java/dev/hardlinkdev/multidb/MultiDbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hardlinkdev.multidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
